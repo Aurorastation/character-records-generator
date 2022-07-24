9b85a836fbedc715b61abc55422a0800c311d820
Llywelwyn <lewiiswynne@gmail.com>
2022-07-24 11:42:32 +0100

    whitespace, comments, broad subtypes
    - changes some whitespace stuff
    - adds some comments for ease of reading
    
    player facing stuff
    - adds hharar, njarir to tajara
    - adds axiori, xiialt, xiori to skrell
    - adds breeders, bulwarks to vaurca
    - adds baseline, bishop to ipc models
    - renames g1, g2 heavy frames to industrial
    - fixes shells being listed twice
    - removes 'do not clone' option

--- AuroraRecordGenerator/RecordFormatterGenerationMethods.cs
+++ AuroraRecordGenerator/RecordFormatterGenerationMethods.cs
@@ -4,209 +4,205 @@ using System.Text;
 
 namespace AuroraRecordGenerator
 {
-	internal partial class RecordFormatter
-	{
-		private void MakeCommonRecords()
-		{
-			var record = new StringBuilder();
-			record.AppendLine("/// PUBLIC RECORD ///");
-			record.AppendLine(MakeNameLine());
-			record.AppendLine($"Date of Birth: {_targetRecord.BirthDate.ToString("MMMM")} {_targetRecord.BirthDate.Day.Ordinalize()}, {_targetRecord.BirthDate.Year}");
-			record.AppendLine($"Species: {_targetRecord.Species.Humanize()}");// might fuck up the names
-			if (_targetRecord.Subspecies != SpeciesSubType.None)
-			{
-				record.AppendLine($"{_targetRecord.Subspecies.GetAttributeOfType<SubspeciesMetaAttribute>()?.FieldName ?? "Subspecies"}: {Utility.SubspeciesNiceName(_targetRecord.Subspecies)}");
-			}
-			record.AppendLine(_targetRecord.Species.HasGender()
-				? $"Gender: {_targetRecord.Gender.Humanize()}"
-				: "Gender: Not Applicable.");
-			record.AppendLine($"Citizenship: {_targetRecord.Citizenship.IfEmpty("Not Specified.")}");
-			record.AppendLine($"Clearance Level: {_targetRecord.Clearance.IfEmpty("Not Specified")}");
-			record.AppendLine($"Employed As: {_targetRecord.EmployedAs.IfEmpty("Assistant")}");
-			if (_targetRecord.CharHeight != null)
-				record.AppendLine($"Height: {_targetRecord.CharHeight} cm ({Utility.CmToFeet(_targetRecord.CharHeight.Value)})");
+    internal partial class RecordFormatter
+    {
+        private void MakeCommonRecords()
+        {
+            var record = new StringBuilder();
+            record.AppendLine("/// PUBLIC RECORD ///");
+            record.AppendLine(MakeNameLine());
+            record.AppendLine($"Date of Birth: {_targetRecord.BirthDate.ToString("MMMM")} {_targetRecord.BirthDate.Day.Ordinalize()}, {_targetRecord.BirthDate.Year}");
+            record.AppendLine($"Species: {_targetRecord.Species.Humanize()}");// might fuck up the names
+            if (_targetRecord.Subspecies != SpeciesSubType.None)
+            {
+                record.AppendLine($"{_targetRecord.Subspecies.GetAttributeOfType<SubspeciesMetaAttribute>()?.FieldName ?? "Subspecies"}: {Utility.SubspeciesNiceName(_targetRecord.Subspecies)}");
+            }
+            record.AppendLine(_targetRecord.Species.HasGender()
+                ? $"Gender: {_targetRecord.Gender.Humanize()}"
+                : "Gender: Not Applicable.");
+            record.AppendLine($"Citizenship: {_targetRecord.Citizenship.IfEmpty("Not Specified.")}");
+            record.AppendLine($"Clearance Level: {_targetRecord.Clearance.IfEmpty("Not Specified")}");
+            record.AppendLine($"Employed As: {_targetRecord.EmployedAs.IfEmpty("Assistant")}");
+            if (_targetRecord.CharHeight != null)
+                record.AppendLine($"Height: {_targetRecord.CharHeight} cm ({Utility.CmToFeet(_targetRecord.CharHeight.Value)})");
 
-			if (_targetRecord.Weight != null)
-				record.AppendLine($"Weight: {_targetRecord.Weight} kg ({Utility.KgToLb(_targetRecord.Weight ?? 0)} lb)");
+            if (_targetRecord.Weight != null)
+                record.AppendLine($"Weight: {_targetRecord.Weight} kg ({Utility.KgToLb(_targetRecord.Weight ?? 0)} lb)");
 
-			// Eye color
-			var trimmedEye = _targetRecord.EyeColor.Trim();
-			record.AppendFormat("Eye Color: {0}\n", trimmedEye.Length > 0 ? trimmedEye : "Not Specified.");
+            // Eye color
+            var trimmedEye = _targetRecord.EyeColor.Trim();
+            record.AppendFormat("Eye Color: {0}\n", trimmedEye.Length > 0 ? trimmedEye : "Not Specified.");
 
-			var bodyColor = _targetRecord.SkinColor.Trim();
-			record.AppendFormat("Skin/Body Color: {0}\n", bodyColor.Length > 0 ? bodyColor : "Not Specified.");
+            var bodyColor = _targetRecord.SkinColor.Trim();
+            record.AppendFormat("Skin/Body Color: {0}\n", bodyColor.Length > 0 ? bodyColor : "Not Specified.");
 
-			var hairColor = _targetRecord.HairColor.Trim();
-			record.AppendFormat("Hair Color: {0}\n", hairColor.Length > 0 ? hairColor : "Not Specified.");
+            var hairColor = _targetRecord.HairColor.Trim();
+            record.AppendFormat("Hair Color: {0}\n", hairColor.Length > 0 ? hairColor : "Not Specified.");
 
-			// identifying features
-			var trimmedFeatures = _targetRecord.DistinguishingFeatures.Trim();
-			record.Append("Distinguishing Features: ");
-			record.AppendLine(trimmedFeatures.Length > 0 ? trimmedFeatures : "None noted.");
+            // identifying features
+            var trimmedFeatures = _targetRecord.DistinguishingFeatures.Trim();
+            record.Append("Distinguishing Features: ");
+            record.AppendLine(trimmedFeatures.Length > 0 ? trimmedFeatures : "None noted.");
 
-			record.AppendLine();
+            record.AppendLine();
 
-			// general notes
-			WriteSectionIfAny(ref record,
-				"General Notes:",
-				_employmentPublicRecord);
+            // general notes
+            WriteSectionIfAny(ref record,
+                "General Notes:",
+                _employmentPublicRecord);
 
-			WriteSectionIfAny(ref record,
-				"Medical Notes:",
-				_medicalPublicRecord);
+            WriteSectionIfAny(ref record,
+                "Medical Notes:",
+                _medicalPublicRecord);
 
-			WriteSectionIfAny(ref record,
-				"Security Notes:",
-				_securityPublicRecord);
+            WriteSectionIfAny(ref record,
+                "Security Notes:",
+                _securityPublicRecord);
 
-			_commonRecords = record.ToString();
-		}
+            _commonRecords = record.ToString();
+        }
 
-		private string MakeEmploymentRecords()
-		{
-			var recordText = new StringBuilder();
-			if (_commonRecords.IsEmpty())
-				MakeCommonRecords();
+        private string MakeEmploymentRecords()
+        {
+            var recordText = new StringBuilder();
+            if (_commonRecords.IsEmpty())
+                MakeCommonRecords();
 
-			recordText.Append(_commonRecords);
+            recordText.Append(_commonRecords);
 
-			if (!_employmentExperience.Any() &&
-				!_employmentFormalEducation.Any() &&
-				!_employmentNtEmployment.Any() &&
-				!_employmentPreNtEmployment.Any() &&
-				!_employmentPublicRecord.Any() &&
-				!_employmentSkills.Any())
-			{
-				recordText.AppendLine("/// NO EMPLOYMENT RECORD FOUND ///");
-			}
-			else
-			{
-				recordText.AppendLine("/// EMPLOYMENT RECORD ///");
-				recordText.AppendLine();
+            if (!_employmentExperience.Any() &&
+                !_employmentFormalEducation.Any() &&
+                !_employmentNtEmployment.Any() &&
+                !_employmentPreNtEmployment.Any() &&
+                !_employmentPublicRecord.Any() &&
+                !_employmentSkills.Any())
+            {
+                recordText.AppendLine("/// NO EMPLOYMENT RECORD FOUND ///");
+            }
+            else
+            {
+                recordText.AppendLine("/// EMPLOYMENT RECORD ///");
+                recordText.AppendLine();
 
-				WriteSectionIfAny(ref recordText,
-					"Experience:",
-					_employmentExperience);
+                WriteSectionIfAny(ref recordText,
+                    "Experience:",
+                    _employmentExperience);
 
-				WriteSectionIfAny(ref recordText,
-					"Formal Education History:",
-					_employmentFormalEducation);
+                WriteSectionIfAny(ref recordText,
+                    "Formal Education History:",
+                    _employmentFormalEducation);
 
-				WriteSectionIfAny(ref recordText,
-					"Pre-NanoTrasen Employment History:",
-					_employmentPreNtEmployment);
+                WriteSectionIfAny(ref recordText,
+                    "Pre-NanoTrasen Employment History:",
+                    _employmentPreNtEmployment);
 
-				WriteSectionIfAny(ref recordText,
-					"NanoTrasen Employment History:",
-					_employmentNtEmployment);
+                WriteSectionIfAny(ref recordText,
+                    "NanoTrasen Employment History:",
+                    _employmentNtEmployment);
 
-				WriteSectionIfAny(ref recordText,
-					"Trained in the following:",
-					_employmentSkills);
-			}
+                WriteSectionIfAny(ref recordText,
+                    "Trained in the following:",
+                    _employmentSkills);
+            }
 
-			return recordText.ToString();
-		}
+            return recordText.ToString();
+        }
 
-		private string MakeMedicalRecords()
-		{
-			var recordText = new StringBuilder();
-			if (_commonRecords.IsEmpty())
-				MakeCommonRecords();
+        private string MakeMedicalRecords()
+        {
+            var recordText = new StringBuilder();
+            if (_commonRecords.IsEmpty())
+                MakeCommonRecords();
 
-			recordText.Append(_commonRecords);
+            recordText.Append(_commonRecords);
 
-			// TODO: make this less horrible
-			if (!_medicalHistory.Any() &&
-				!_medicalNotes.Any() &&
-				!_medicalPsychHistory.Any() &&
-				!_medicalPsychNotes.Any() &&
-				!_medicalPrescriptions.Any() &&
-				!_targetRecord.NoBorg &&
-				!_targetRecord.NoClone &&
-				!_targetRecord.NoProsthetic &&
-				!_targetRecord.NoRevive)
-			{
-				recordText.AppendLine("/// NO MEDICAL RECORD FOUND ///");
-			}
-			else
-			{
-				recordText.AppendLine("/// MEDICAL RECORD ///");
-				recordText.AppendLine();
+            // TODO: make this less horrible
+            if (!_medicalHistory.Any() &&
+                !_medicalNotes.Any() &&
+                !_medicalPsychHistory.Any() &&
+                !_medicalPsychNotes.Any() &&
+                !_medicalPrescriptions.Any() &&
+                !_targetRecord.NoBorg &&
+                !_targetRecord.NoProsthetic &&
+                !_targetRecord.NoRevive)
+            {
+                recordText.AppendLine("/// NO MEDICAL RECORD FOUND ///");
+            }
+            else
+            {
+                recordText.AppendLine("/// MEDICAL RECORD ///");
+                recordText.AppendLine();
 
-				recordText.AppendLine(
-					" The following information is protected by doctor-patient confidentiality laws. Do not release without patient's consent.\n");
+                recordText.AppendLine(
+                    " The following information is protected by doctor-patient confidentiality laws. Do not release without patient's consent.\n");
 
-				if (_targetRecord.NoBorg || _targetRecord.NoClone || _targetRecord.NoProsthetic || _targetRecord.NoRevive)
-				{
-					recordText.AppendLine("IMPORTANT NOTES:");
+                if (_targetRecord.NoBorg || _targetRecord.NoProsthetic || _targetRecord.NoRevive)
+                {
+                    recordText.AppendLine("IMPORTANT NOTES:");
 
-					if (_targetRecord.NoBorg)
-						MakeMedicalNote(ref recordText, "DO NOT BORGIFY");
+                    if (_targetRecord.NoBorg)
+                        MakeMedicalNote(ref recordText, "DO NOT BORGIFY");
 
-					if (_targetRecord.NoClone)
-						MakeMedicalNote(ref recordText, "DO NOT CLONE");
+                    if (_targetRecord.NoProsthetic)
+                        MakeMedicalNote(ref recordText, "DO NOT INSTALL PROSTHETICS");
 
-					if (_targetRecord.NoProsthetic)
-						MakeMedicalNote(ref recordText, "DO NOT INSTALL PROSTHETICS");
+                    if (_targetRecord.NoRevive)
+                        MakeMedicalNote(ref recordText, "DO NOT REVIVE");
 
-					if (_targetRecord.NoRevive)
-						MakeMedicalNote(ref recordText, "DO NOT REVIVE");
+                    recordText.AppendLine();
+                }
 
-					recordText.AppendLine();
-				}
+                WriteSectionIfAny(ref recordText,
+                    "Notes:",
+                    _medicalNotes);
 
-				WriteSectionIfAny(ref recordText,
-					"Notes:",
-					_medicalNotes);
+                WriteSectionIfAny(ref recordText,
+                    "Medical History:",
+                    _medicalHistory);
 
-				WriteSectionIfAny(ref recordText,
-					"Medical History:",
-					_medicalHistory);
+                WriteSectionIfAny(ref recordText,
+                    "Psychiatric Notes:",
+                    _medicalPsychNotes);
 
-				WriteSectionIfAny(ref recordText,
-					"Psychiatric Notes:",
-					_medicalPsychNotes);
+                WriteSectionIfAny(ref recordText,
+                    "Psychiatric History:",
+                    _medicalPsychHistory);
 
-				WriteSectionIfAny(ref recordText,
-					"Psychiatric History:",
-					_medicalPsychHistory);
+                WriteSectionIfAny(ref recordText,
+                    "Prescriptions:",
+                    _medicalPrescriptions);
+            }
 
-				WriteSectionIfAny(ref recordText,
-					"Prescriptions:",
-					_medicalPrescriptions);
-			}
+            return recordText.ToString();
+        }
 
-			return recordText.ToString();
-		}
+        private string MakeSecurityRecords()
+        {
+            var recordText = new StringBuilder();
+            if (_commonRecords.IsEmpty())
+                MakeCommonRecords();
 
-		private string MakeSecurityRecords()
-		{
-			var recordText = new StringBuilder();
-			if (_commonRecords.IsEmpty())
-				MakeCommonRecords();
+            recordText.Append(_commonRecords);
 
-			recordText.Append(_commonRecords);
+            if (!_securityRecords.Any() && !_securityNotes.Any())
+            {
+                recordText.AppendLine("/// NO SECURITY RECORD FOUND ///");
+            }
+            else
+            {
+                recordText.AppendLine("/// SECURITY RECORD ///");
+                recordText.AppendLine();
 
-			if (!_securityRecords.Any() && !_securityNotes.Any())
-			{
-				recordText.AppendLine("/// NO SECURITY RECORD FOUND ///");
-			}
-			else
-			{
-				recordText.AppendLine("/// SECURITY RECORD ///");
-				recordText.AppendLine();
+                WriteSectionIfAny(ref recordText,
+                    "Notes:",
+                    _securityNotes);
 
-				WriteSectionIfAny(ref recordText,
-					"Notes:",
-					_securityNotes);
+                WriteSectionIfAny(ref recordText,
+                    "Record:",
+                    _securityRecords);
+            }
 
-				WriteSectionIfAny(ref recordText,
-					"Record:",
-					_securityRecords);
-			}
-
-			return recordText.ToString();
-		}
-	}
+            return recordText.ToString();
+        }
+    }
 }
--- AuroraRecordGenerator/Types.cs
+++ AuroraRecordGenerator/Types.cs
@@ -3,114 +3,127 @@ using System;
 
 namespace AuroraRecordGenerator
 {
-	[ProtoContract]
-	public enum SpeciesType
-	{
-		[ProtoEnum]
-		None = 0,
+    [ProtoContract]
+    public enum SpeciesType
+    {
+        [ProtoEnum]
+        None = 0,
 
-		[ProtoEnum]
-		Human,
+        [ProtoEnum]
+        Human,
 
-		[ProtoEnum]
-		Skrell,
+        [ProtoEnum]
+        Skrell,
 
-		[ProtoEnum]
-		Tajara,
+        [ProtoEnum]
+        Tajara,
 
-		[ProtoEnum]
-		Unathi,
+        [ProtoEnum]
+        Unathi,
 
-		[ProtoEnum]
-		Vaurca,
+        [ProtoEnum]
+        Vaurca,
 
-		[ProtoEnum]
-		Diona,
+        [ProtoEnum]
+        Diona,
 
-		[ProtoEnum]
-		IPC
-	}
+        [ProtoEnum]
+        IPC
+    }
 
-	[ProtoContract]
-	public enum SpeciesSubType
-	{
-		[ProtoEnum, SubspeciesMeta(SpeciesType.None, "N/A")]
-		None = 0,
+    [ProtoContract]
+    public enum SpeciesSubType
+    {
+        [ProtoEnum, SubspeciesMeta(SpeciesType.None, "N/A")]
+        None = 0,
 
-		[ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "M'sai", "Ethnicity")]
-		MsaiTajara,
+        // SKRELL VARIANTS
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Skrell, "Axiori", "Ethnicity")]
+        SkrellAxiori,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Skrell, "Xiialt", "Ethnicity")]
+        SkrellXiialt,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Skrell, "Xiiori", "Ethnicity")]
+        SkrellXiiori,
 
-		[ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "Zhan-Khazan", "Ethnicity")]
-		ZhanTajara,
+        // TAJARA VARIANTS
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "Hharar", "Ethnicity")]
+        TajaraHharar,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "Zhan-Khazan", "Ethnicity")]
+        TajaraZhan,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "Njarir'Akhran", "Ethnicity")]
+        TajaraNjarir,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Tajara, "M'sai", "Ethnicity")]
+        TajaraMsai,
 
-		[ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type A (Worker)", "Classification")]
-		VaurcaWorker,
+        // VAURCA VARIANTS
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type A (Worker)", "Classification")]
+        VaurcaWorker,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type B (Warrior)", "Classification")]
+        VaurcaWarrior,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type C (Breeder)", "Classification")]
+        VaurcaBreeder,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type E (Bulwark)", "Classification")]
+        VaurcaBulwark,
 
-		[ProtoEnum, SubspeciesMeta(SpeciesType.Vaurca, "Type B (Warrior)", "Classification")]
-		VaurcaWarrior,
-
-		[ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Shell", "Model")]
-		IpcShell,
-
-		[ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Hephaestus G1 Heavy", "Model")]
-		IpcG1Industrial,
-
-        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Hephaestus G2 Heavy", "Model")]
-        IpcG2Industrial,
-
-        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Bishop Accessory", "Model")]
-        IpcFancy,
-
-        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Zeng-Hu Mobility", "Model")]
-        IpcMedsci,
-
+        // IPC VARIANTS
+        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Baseline", "Model")]
+        IpcBaseline,
         [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Shell", "Model")]
-        IpcHumanoid,
-
+        IpcShell,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Hephaestus G1 Industrial", "Model")]
+        IpcG1,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Hephaestus G2 Industrial", "Model")]
+        IpcG2,
         [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Xion Industrial", "Model")]
-        IpcHeavy,
+        IpcXion,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Zeng-Hu Mobility", "Model")]
+        IpcZengHu,
+        [ProtoEnum, SubspeciesMeta(SpeciesType.IPC, "Bishop Accessory", "Model")]
+        IpcBishop,
 
+        // UNATHI VARIANTS
         [ProtoEnum, SubspeciesMeta(SpeciesType.Unathi, "Aut'akh", "Variant")]
-        UnathiRobot,
+        UnathiAutakh,
 
+        // HUMAN VARIANTS
         [ProtoEnum, SubspeciesMeta(SpeciesType.Human, "Offworlder", "Variant")]
         HumanOffworld
-	}
+    }
 
-	[ProtoContract]
-	public enum GenderType
-	{
-		[ProtoEnum]
-		NotApplicable = 0,
+    [ProtoContract]
+    public enum GenderType
+    {
+        [ProtoEnum]
+        NotApplicable = 0,
 
-		[ProtoEnum]
-		Male,
+        [ProtoEnum]
+        Male,
 
-		[ProtoEnum]
-		Female
-	}
+        [ProtoEnum]
+        Female
+    }
 
-	public static class Info
-	{
-		/// <summary>
-		/// The current in-character date.
-		/// </summary>
-		public static DateTime IcDate => new DateTime(DateTime.Now.Year + 442,
-			DateTime.Now.Month,
-			DateTime.Now.Day);
-	}
+    public static class Info
+    {
+        /// <summary>
+        /// The current in-character date.
+        /// </summary>
+        public static DateTime IcDate => new DateTime(DateTime.Now.Year + 442,
+            DateTime.Now.Month,
+            DateTime.Now.Day);
+    }
 
-	[AttributeUsage(AttributeTargets.Field)]
-	public class SubspeciesMetaAttribute : Attribute
-	{
-		public SpeciesType AssociatedSpecies {get; private set;}
-		public string NiceName { get; private set; }
-		public string FieldName { get; private set; }
-		public SubspeciesMetaAttribute(SpeciesType associatedType, string nicename, string fieldname = "Subspecies")
-		{
-			AssociatedSpecies = associatedType;
-			NiceName = nicename;
-			FieldName = fieldname;
-		}
-	}
+    [AttributeUsage(AttributeTargets.Field)]
+    public class SubspeciesMetaAttribute : Attribute
+    {
+        public SpeciesType AssociatedSpecies { get; private set; }
+        public string NiceName { get; private set; }
+        public string FieldName { get; private set; }
+        public SubspeciesMetaAttribute(SpeciesType associatedType, string nicename, string fieldname = "Subspecies")
+        {
+            AssociatedSpecies = associatedType;
+            NiceName = nicename;
+            FieldName = fieldname;
+        }
+    }
 }
